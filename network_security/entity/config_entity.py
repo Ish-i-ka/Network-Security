ac7b0ccec48612c4120d4225772357d9c76dfb82
Is-hi-ka <ishika.sahajuly21@gmail.com>
2025-07-03 23:45:08 +0530

    Data Transformation Component added

--- network_security/entity/config_entity.py
+++ network_security/entity/config_entity.py
@@ -2,8 +2,8 @@ from datetime import datetime
 import os
 from network_security.constant import training_pipeline
 
-print(training_pipeline.PIPELINE_NAME)
-print(training_pipeline.ARTIFACT_DIR)
+'''print(training_pipeline.PIPELINE_NAME)
+print(training_pipeline.ARTIFACT_DIR)'''
 
 
 class TrainingPipelineConfig:
@@ -55,7 +55,7 @@ class DataValidationConfig:
         )
 
 
-'''class DataTransformationConfig:
+class DataTransformationConfig:
      def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.data_transformation_dir: str = os.path.join( training_pipeline_config.artifact_dir,training_pipeline.DATA_TRANSFORMATION_DIR_NAME )
         self.transformed_train_file_path: str = os.path.join( self.data_transformation_dir,training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
@@ -65,7 +65,7 @@ class DataValidationConfig:
         self.transformed_object_file_path: str = os.path.join( self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
             training_pipeline.PREPROCESSING_OBJECT_FILE_NAME,)
         
-class ModelTrainerConfig:
+'''class ModelTrainerConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.model_trainer_dir: str = os.path.join(
             training_pipeline_config.artifact_dir, training_pipeline.MODEL_TRAINER_DIR_NAME
--- network_security/utils/main_utils/utils.py
+++ network_security/utils/main_utils/utils.py
@@ -30,3 +30,35 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
             yaml.dump(content, file)
     except Exception as e:
         raise NetworkSecurityException(e, sys)
+    
+def save_numpy_array_data(file_path: str, array: np.ndarray):
+    """
+    Saves a NumPy array to a file.
+    
+    :param file_path: Path to the file where the array will be saved.
+    :param array: NumPy array to be saved.
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+    
+def save_object(file_path: str, obj: object) -> None:
+    """
+    Saves an object to a file using dill.
+    
+    :param file_path: Path to the file where the object will be saved.
+    :param obj: Object to be saved.
+    """
+    try:
+        logging.info("Entered save_object method of main_utils/utils.py")
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            pickle.dump(obj, file_obj)
+        logging.info(f"Object saved at {file_path}")
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
